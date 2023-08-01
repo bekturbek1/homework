dc25ae98576b67203c1aaf07c4044ba1ffd06831
bekturbek1 <nurlan.arstanbekov8@gmail.com>
2023-08-01 17:33:33 +0600

    first commit

---
+++ less11.js
@@ -0,0 +1,190 @@
+let ff = alert ("слова из 8 букв и 1 цифры")
+
+
+let sec = 'b'; 
+
+let guf = false;
+
+ while (!guf) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec) { alert("Вы угадали!");
+  
+ guf = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec );
+
+
+
+
+ let sec1 = 'e'; 
+
+ let guf1 = false;
+ 
+  while (!guf1) { let userInput = prompt("Угадайте секретную букву:");
+ 
+  if (userInput === sec1) { alert("Вы угадали!");
+   
+  guf1 = true; }
+  
+  else { alert("Вы не угадали. Попробуйте еще раз."); } }
+ 
+  console.log( "вы угадали еще 1 букву" ,sec1 );
+
+
+
+  let sec2 = 'k'; 
+
+  let guf2 = false;
+  
+   while (!guf2) { let userInput = prompt("Угадайте секретную букву:");
+  
+   if (userInput === sec2) { alert("Вы угадали!");
+    
+   guf2 = true; }
+   
+   else { alert("Вы не угадали. Попробуйте еще раз."); } }
+  
+   console.log( "вы угадали еще 1 букву" ,sec2 );
+
+
+   
+  let sec3 = 't'; 
+
+  let guf3 = false;
+  
+   while (!guf3) { let userInput = prompt("Угадайте секретную букву:");
+  
+   if (userInput === sec3) { alert("Вы угадали!");
+    
+   guf3 = true; }
+   
+   else { alert("Вы не угадали. Попробуйте еще раз."); } }
+  
+   console.log( "поздравляю вы угадали 1 букву" ,  );
+
+
+
+   let sec4 = 'u'; 
+
+let guf4 = false;
+
+ while (!guf4) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec4) { alert("Вы угадали!");
+  
+ guf4 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec4 );
+
+
+let sec5 = 'r'; 
+
+let guf5 = false;
+
+ while (!guf5) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec5) { alert("Вы угадали!");
+  
+ guf5 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec5 );
+
+
+let sec6 = 'b'; 
+
+let guf6 = false;
+
+ while (!guf6) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec6) { alert("Вы угадали!");
+  
+ guf6 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec6 );
+
+
+
+let sec7 = 'e'; 
+
+let guf7 = false;
+
+ while (!guf7) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec7) { alert("Вы угадали!");
+  
+ guf7 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec7 );
+
+
+
+
+
+let sec8 = 'k'; 
+
+let guf8 = false;
+
+ while (!guf8) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec8) { alert("Вы угадали!");
+  
+ guf8 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали 1 букву" ,sec8 );
+
+
+
+
+let sec9 = '1'; 
+
+let guf9 = false;
+
+ while (!guf9) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec9) { alert("Вы угадали!");
+  
+ guf9 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали цифру 1" , sec9  );
+
+
+
+
+
+let sec10 = 'bekturbek1'; 
+
+let guf10 = false;
+
+ while (!guf10) { let userInput = prompt("Угадайте секретную букву:");
+
+ if (userInput === sec10) { alert("Вы угадали!");
+  
+ guf10 = true; }
+ 
+ else { alert("Вы не угадали. Попробуйте еще раз."); } }
+
+console.log( "вы угадали слова bekturbek1"  );
+
+
+
+
+
+
+
+
+
+
